43fefbb9eb5b9b70a6ce3fd3b910f971038d62c5
Shashi Singh <101687956+shashirvce@users.noreply.github.com>
2022-09-04 13:07:33 +0530

    Add files via upload

---
+++ ImplicitTypeCasting.java
@@ -0,0 +1,24 @@
+public class ImplicitTypeCasting 
+{
+    public static void main(String[] args)
+    {
+        byte a=10;
+        int  b;
+        float c=20.6f;
+        double d;
+        short e;
+        long f;
+        //implicit Type Casting Example
+        b=a;
+        d=c;
+        e=a;
+        f=a;
+        System.out.println("mplicit Type Casting Example");
+        System.out.println("Value Of A:"+a);
+        System.out.println("Value Of B:"+b);
+        System.out.println("Value Of C:"+c);
+        System.out.println("Value Of D:"+d);
+        System.out.println("Value Of E:"+e);
+        System.out.println("Value Of F:"+f);
+    }
+}

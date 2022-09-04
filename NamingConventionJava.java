e542f2d26a2213a341f91fe22876b20b41e306c5
Shashi Singh <101687956+shashirvce@users.noreply.github.com>
2022-09-04 13:28:49 +0530

    Add files via upload

---
+++ NamingConventionJava.java
@@ -0,0 +1,11 @@
+public class NamingConventionJava
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Class Name Start With Capital Letters");
+        System.out.println("Variable And Function Can Start With Small Letters");
+        System.out.println("It Should Not Start With A Number");
+        System.out.println("It Should Follow CamelCase in Java Naming Conventions");
+        System.out.println("EXAMPLE:\nCLASS NAME:HelloAliean\nVARAIBLE NAME:String aliean;\nMEATHOD NAME:getAlieanName();");
+    }
+}

e85893429d4ad436da5040be257b0bd6c00225f6
Shashi Singh <101687956+shashirvce@users.noreply.github.com>
2022-09-04 12:52:35 +0530

    Add files via upload

---
+++ AddTwoNumber.java
@@ -0,0 +1,9 @@
+public class AddTwoNumber 
+{
+    public static void main(String[] args)
+    {
+        byte a=10;
+        byte b=20;
+        System.out.println("Addtion Of "+a+" and "+b+" is:"+(a+b));
+    }
+}
